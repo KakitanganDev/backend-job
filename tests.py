167396c6f5e3ec9327fe7d363f4d693b1a697912
zakwan hisham <hishamzakwan@gmail.com>
2024-08-29 20:26:23 +0800

    Fix JSONDecoding Error and Update Tests
    - Fixed JSON string in main.py from "sellus": False to "sellus": false.
    - Update tests.py to check for successfull JSON decoding instead of
      expecting an error

--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
 import unittest
+import main
+import json
 
 class TestGeneral(unittest.TestCase):
-   def test_placeholder(self):
-       self.assertTrue(True)
+    def test_placeholder(self):
+        try:
+            data = json.loads(main.get_data())
+            self.assertIsInstance(data, dict)
+        except json.JSONDecodeError:
+            self.fail("json.JSONDecodeError was raised unexpectedly!")
+
+if __name__ == "__main__":
+    unittest.main()
