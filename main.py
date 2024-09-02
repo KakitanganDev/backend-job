9f088b473ddca026e618caefc7a99c596fb87d8c
Azlina Manap <55196677+azlinamanap@users.noreply.github.com>
2024-09-02 16:19:46 +0800

    Fixed the error: Line 4 'False' changed to 'false'. Added a unit test.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 def get_data():
-    return '{"tiam": "bibendum", "lacus": 23.5, "sellus": False}'
+    return '{"tiam": "bibendum", "lacus": 23.5, "sellus": false}'
 
 def main():
     data = json.loads(get_data())
--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
 import unittest
+import json
+from main import get_data
 
 class TestGeneral(unittest.TestCase):
-   def test_placeholder(self):
-       self.assertTrue(True)
+   
+    def test_valid_json(self):
+        data_str = get_data()
+
+        try:
+            json_data = json.loads(data_str)
+        
+        except json.JSONDecodeError as message:
+            self.fail(f"Invalid JSON: {message}")
