c964c225409ed6167ae5b0794dff561bca06c539
Azlina Manap <55196677+azlinamanap@users.noreply.github.com>
2024-09-02 18:02:02 +0800

    Added comment in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
 def get_data():
+    #False changed to false: JSON and Python uses different capitalisation
     return '{"tiam": "bibendum", "lacus": 23.5, "sellus": false}'
 
 def main():
